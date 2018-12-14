5371fec4883d29237b8d5704f0ca57fa017d5792
zhenan.wang <2513951676@qq.com>
2018-12-14 09:30:55 +0800

    2018.12.15 work

---
+++ build/web/websocket.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var wsUri = "ws://" + document.location.host + document.location.pathname +
+        "collaboratveBoardEndPoint";
+var websocket = new WebSocket(wsUri);
+
+websocket.onerror = function(evt) { 
+    onError(evt);
+};
+
+function onError (evt) {
+    writeToScreen('<span style="color: red;"> ERROR: </span' + evt.data);
+}
